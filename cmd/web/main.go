27c8dc7323f29bdb046dbda40b6c3bc8841e50b5
xmechaniqx <vepserf@yandex.ru>
2023-08-22 19:15:20 +0400

    1.2.2

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,45 +9,32 @@ import (
 )
 
 func main() {
-	// Используется функция http.NewServeMux() для инициализации нового рутера, затем
-	// функцию "home" регистрируется как обработчик для URL-шаблона "/".
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 	mux.HandleFunc("/flag", showFlag)
-	// mux.Handle("/ui/static/img/", http.StripPrefix("/ui/static/img/", http.FileServer(http.Dir("./img"))))
-	// mux.HandleFunc("/img", func(w http.ResponseWriter, r *http.Request) {
-	// 	http.ServeFile(w, r, "base.layout.html")
-	// })
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// fmt.Println(fileServer)
-	// fileServer := http.FileServer(http.Dir("C:/workspace"))
-	// mux.Handle("/#/", http.StripPrefix("/#", fileServer))
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-	// vfs.DirLook(&"s")
 }
 func showFlag(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	// w.Header().Set("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/json")
 	root := r.URL.Query().Get("root")
 	fmt.Println(root)
-	returner, _, err := vfs.DirLook(root)
+	returner, err := vfs.DirLook(root)
 	if err != nil {
 		fmt.Println("Ошибка функции vfs.DirLook")
 	}
-	// fmt.Println(returner)
-	// json.NewEncoder(w).Encode(r)
-	output, err := json.MarshalIndent(returner, "", "")
+	output, err := json.Marshal(returner)
+	// JsonOutput, err := json.MarshalIndent(returner, "", "\t")
 	if err != nil {
-		fmt.Println("Marshall")
+		fmt.Println("Can't Marshall JSON")
 	}
-
 	w.Write(output)
 }
 
--- cmd/web/vfs/vfs.go
+++ cmd/web/vfs/vfs.go
@@ -11,17 +11,18 @@ import (
 type VFSNode struct {
 	Path string  `json:"path"`
 	Size float64 `json:"size"`
+	Stat string  `json:"stat"`
 }
 
 type MainVFS struct {
 	Duration time.Duration `json:"time"`
 	Root     string        `json:"root"`
-	Node     []VFSNode     `json:"VFSNode struct"`
+	Node     []VFSNode     `json:"VFSNode_struct"`
 }
 
 //Терминальная утилита RBS-EX2.3 используется для анализа размера содержимого для указанной директории.
 
-func DirLook(root string) ([]VFSNode, []MainVFS, error) {
+func DirLook(root string) (MainVFS, error) {
 	start := time.Now()
 	// root = flag.String("root", "", "path")
 	// flag.Parse()
@@ -39,32 +40,34 @@ func DirLook(root string) ([]VFSNode, []MainVFS, error) {
 	for _, file := range files {
 		filesOfDir = append(filesOfDir, filepath.Join(path, file.Name()))
 	}
-
 	vfsNodes := make([]VFSNode, 0)
-	mainVFS := make([]MainVFS, 0)
 	var wg sync.WaitGroup
 	wg.Add(len(filesOfDir))
 	for _, dirEntered := range filesOfDir {
 
 		go func(dirEntered string) {
 			defer wg.Done()
-			size := dirSize(dirEntered)
-			vfsNodes = append(vfsNodes, VFSNode{Path: dirEntered, Size: size})
+			size, stat := dirSize(dirEntered)
+			vfsNodes = append(vfsNodes, VFSNode{Path: dirEntered, Size: size, Stat: stat})
 
 		}(dirEntered)
 	}
 	wg.Wait()
 
-	fmt.Println(duration)
-	mainVFS = []MainVFS{{Duration: duration}, {Root: root}, {Node: vfsNodes}}
-	return vfsNodes, mainVFS, nil
+	MyMainVFS := MainVFS{
+		Duration: duration,
+		Root:     root,
+		Node:     vfsNodes,
+	}
+	return MyMainVFS, nil
 }
 
 /*dirSize() функция принимает путь к директории, определяет тип содержимого (файл или папка)
 и возвращает размер содержимого для файла либо сумму размеров содержимого для папки*/
-func dirSize(path string) float64 {
+func dirSize(path string) (float64, string) {
 	sizes := make(chan int64)
-	// booler := make(chan bool)
+
+	var stat string
 	size := int64(0)
 	readSize := func(path string, file os.FileInfo, err error) error {
 		if err != nil || file == nil {
@@ -72,9 +75,10 @@ func dirSize(path string) float64 {
 		}
 		if !file.IsDir() {
 			// fmt.Println(path, file.Size())
+			stat = "file"
 			sizes <- file.Size()
 		} else {
-			// if file.IsDir() {
+			stat = "dir"
 			size = file.Size()
 		}
 		return err
@@ -86,6 +90,9 @@ func dirSize(path string) float64 {
 	for s := range sizes {
 		size += s
 	}
-	return float64(size)
+	// fmt.Println(stat, path)
+	return float64(size), stat
 
 }
+
+// http://127.0.0.1:4000/flag?root=/home/username/
