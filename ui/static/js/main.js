a81a2b5fa129b351e4d4b1ed7e534337ee2275f7
xmechaniqx <vepserf@yandex.ru>
2023-08-23 15:26:41 +0400

    1.2.5

--- ui/static/js/main.js
+++ ui/static/js/main.js
@@ -1,57 +1,44 @@
 console.log("JS loaded")
-var url = window.location.href + 'flag?root=C:/workspace/';
-// var url = window.location.href + 'flag?root=/home/username/Загрузки';
-console.log("url is",url)
+// var url = window.location.href + 'flag?root=C:/workspace/';
+var defaultURL = window.location.href + 'flag?root=/home/username/Загрузки';
 var currentLocation = window.location.href;
 
- function FnRequest(){
-  var req = new XMLHttpRequest();
-req.addEventListener("load", renderResponse);
-req.open("GET", url);
-req.send();
+FnRequest(defaultURL)
+// renderResponse()
+function FnRequest(url){
+  var req = new XMLHttpRequest(url);
+  req.addEventListener("load", renderResponse);
+  req.open("GET", url);
+  req.send();
 }
-FnRequest()
-
 
 function renderResponse() {
   const resp = JSON.parse(this.response)  
   const ul = document.getElementById("result")
-  console.log(ul)
+  ul.innerHTML = ''
   resp.VFSNode_struct.forEach(element => {
-    console.log("element",element.path);
-    const li = document.createElement("li")
-    console.log(li)
-
-    // console.log(element.stat)
-// FnRequest()
-    // if  (String(element.stat).valueOf() == String("file").valueOf()) {
-     
-      // li.innerHTML = '<a class="#"><div class="results"><img src="/static/img/file.png'+element.path +'</div></a>' 
-    // } else if (String(element.stat).valueOf() == String("dir").valueOf()){
-      let newUrl=element.path
-      
-      
-      console.log(newUrl.replace('ForwardSlash',/\//g)+'/')
+  const li = document.createElement("li")
 
 
-      li.innerHTML = '<a class="#" href='+newUrl+'><div class="results">'+element.path +'</div></a>' ;
-    // }
-    
+  
+  li.innerHTML = '<span ><div class="results"><img src="/static/img/folder.png" width="1%">'+element.path +'</div></span>';
 
-    // renderResponse()
-    // li.innerHTML = '<a class="#"><div class="results">'+element.path +'</div></a>'  
-    ul.appendChild(li);
+      let newUrl=window.location.href + 'flag?root='+element.path+'/'
 
+li.addEventListener("click", () => {
+  FnRequest(newUrl)
+},  false,)    
+  ul.appendChild(li);
   });
 }
 
-// renderResponse()
-function click(){
-  const ul = document.getElementById("click")
-  document.addEventListener("mouseup",
-                ()=>console.log("mouseup"));
-                url= "123"
-                console.log(url)
-                FnRequest(url)
-                renderResponse(url)
-}
+
+const goBack=document.getElementById("goback").addEventListener("click", () => {
+  // const li = document.createElement("li")
+  // alert("puk")
+  console.log("catch a click")
+
+  
+  // }
+  
+},  false,) 
